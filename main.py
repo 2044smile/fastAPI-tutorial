46a3edc32d4c98dcae632766db62cf5508bedd4a
2044smile <2044smile@naver.com>
2022-06-24 11:32:35 +0900

    feat: 3.8~9 and 3.10 different

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Union
+from datetime import datetime
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -10,6 +11,27 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+
+
+class User(BaseModel):
+    id: int
+    name = "cs lee"
+    # name: str = "cs lee"  str 표시의 유무
+    signup_ts: datetime | None = None  # 3.10 Version Not Union
+    # signup_same: Union[datetime, None] = None  # 3.8, 9 Version
+    friends: list[int] = []  # [int, str] str 은 기본 내장
+
+
+external_date = {
+    "id": "1004",
+    "signup_ts": "2022-06-24 12:00",
+    "friends": [1, "2", b"3"]
+}
+
+
+user = User(**external_date)
+print(user)
+print(user.id)
 
 
 @app.get("/")
